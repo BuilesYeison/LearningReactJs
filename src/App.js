6b7ab7f8c408035883b8d2ab8eb2bb89ea4d2eb2
Yeison Stiven Builes Uribe <yeisonplox@gmail.com>
2021-04-18 12:54:32 -0500

    Primeros pasos con react: creando un login e implementando sass, css react, google fonts y materialize al proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/login.css';
+import {useState} from "react";
 
 function App() {
+  const [email,setEmail] = useState();
+  const [password,setPassw] = useState();
+
+  const handleEmailEvent = evt=>setEmail(evt.target.value)
+  const handlePasswordEvent = ({target:{value}}) =>setPassw(value)//desestructuracion
+  const handleFormSubmit = (event) =>{
+    event.preventDefault();//por default el boton al hacer submit refresca la pagina, con esta linea d codigo resteamos esa accion por default e impedimos que lo haga
+    console.log(`Correo: ${email} password: ${password}`)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleFormSubmit} className="content">
+        <h2>Iniciar Sesión</h2>
+        <div className="input-field">
+          <input value={email} onChange={handleEmailEvent} id="emailInput" type="email" className="validate"/>
+          <label className="active" for="emailInput">Email</label>
+        </div>
+        <div className="input-field">
+          <input value={password} onChange={handlePasswordEvent} id="passwordInput" type="password" className="validate"/>
+          <label className="active" for="passwordInput">Password</label>
+        </div>
+        <button type="submit">Enter</button>
+      </form>
     </div>
   );
 }
